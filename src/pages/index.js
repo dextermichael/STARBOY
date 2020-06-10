1dbdfa77043814a0d8908853c30f9523ba498db3
Dexter Hennington <dextermichael.h@gmail.com>
2020-06-10 14:38:32 -0400

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome to STAR BOY!!</h1>
-    <p>Follow our journey</p>
-    <p>As we build something great!</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Welcome to STAR BOY!!</h1>
+        <p>Follow our journey</p>
+        <p>As we build something great!</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
