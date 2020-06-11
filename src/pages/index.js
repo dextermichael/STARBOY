d7270026c50b00f761c84cdc81062f33dfe4f8cc
Dexter Hennington <dextermichael.h@gmail.com>
2020-06-11 11:03:13 -0400

    transitions

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Welcome to STAR BOY!!</h1>
+        <h1>
+          Welcome to <br /> STAR BOY!!
+        </h1>
         <p>Follow our journey</p>
         <p>As we build something great!</p>
         <Link to="/page-2/">Go to page 2</Link>
