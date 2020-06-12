fcf88c8336725c4bfabb4a8d8c3aad9018ed358b
Dexter Hennington <dextermichael.h@gmail.com>
2020-06-12 13:45:37 -0400

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
-import React from 'React'
+import React from 'react'
 import styled from 'styled-components'
-import Link from 'gatsby-link'
 
 const FooterGroup = styled.div`
   background: #f1f3f5;
@@ -8,7 +7,6 @@ const FooterGroup = styled.div`
   display: grid;
   grid-gap: 20px;
 `
-
 const Text = styled.p`
   font-size: 24px;
   font-weight: 600;
@@ -16,49 +14,40 @@ const Text = styled.p`
   max-width: 500px;
   margin: 0 auto;
 `
-
 const Button = styled.button`
-  font-size: 24px;
+  background: linear-gradient(102.24deg, #9b51e0 0%, #3436e7 100%);
+  box-shadow: 0px 10px 20px rgba(101, 41, 255, 0.15);
+  border-radius: 30px;
   color: white;
   border: none;
   padding: 16px 60px;
   font-weight: 600;
-  background: linear-gradient(102.24deg, #9b51e0 0%, #3436e7 100%);
-  box-shadow: 0px 10px 20px rgba(101, 41, 255, 0.15);
-  border-radius: 30px;
+  font-size: 24px;
   justify-self: center;
   transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-
   &:hover {
     box-shadow: 0 20px 40px rgba(0, 0, 0, 0.15);
     transform: translateY(-3px);
   }
 `
-
-let LinkGroup = styled.div`
+const LinkGroup = styled.div`
   width: 500px;
   margin: 50px auto;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 10px;
-
   a {
-    color: #5334f5;
-    font-size: 20px;
-    font-weight: 500;
-    margin: 5px 0;
-    transition: 1s;
+    transition: 0.8s;
   }
-
   a:hover {
     color: black;
   }
 `
 
-const Footer = ({ data }) => (
+const Footer = ({ data, children }) => (
   <FooterGroup>
-    <Text> Tweet me "@dexboygenius" </Text>
-    <Button> Tweet</Button>
+    <Text>Tweet @Dexboygenius</Text>
+    <Button>Tweet</Button>
     <LinkGroup>
       {data.allContentfulLink.edges.map((edge) => (
         <a href={edge.node.url}>{edge.node.title}</a>
