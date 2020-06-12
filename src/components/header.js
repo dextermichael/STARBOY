e3b81104ff6e000b797f1f51dfd262f897e673e0
Dexter Hennington <dextermichael.h@gmail.com>
2020-06-11 21:06:56 -0400

    trying to fix logo

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
       <Link to="/">
-        <img src="" width="30/>" />
+        <img src="" width="30" />
       </Link>
       <Link to="/About">About</Link>
       <Link to="/covers">Covers</Link>
