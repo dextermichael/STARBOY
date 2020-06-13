af3bf61cae8bba00db35804df03834a392e843c9
Dexter Hennington <dextermichael.h@gmail.com>
2020-06-13 13:58:41 -0400

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,6 +52,14 @@ const IndexPage = () => (
       </div>
     </div>
     <Section image={require('../images/wallpaper10.jpg')} />
+
+    <SectionCaption> Hobbies</SectionCaption>
+    <SectionCellGroup>
+      {' '}
+      {staticdata.cells.map((cell) => (
+        <Cell title={cell.title} image={cell.image} />
+      ))}
+    </SectionCellGroup>
   </div>
 )
 
